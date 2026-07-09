a4d7a23c33c2af75ba1ce374d728297c08fe9203
debajyotid4s <debajyotid212@gmail.com>
2026-07-10 01:12:18 +0600

    add Vercel deployment config (api/index.js, vercel.json, root package.json)

--- server/src/app.js
+++ server/src/app.js
@@ -64,8 +64,10 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT} *`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT} *`);
+  });
+}
 
 module.exports = app;
